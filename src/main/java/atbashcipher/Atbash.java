8fda6fcdd6a3ed72afd27705a7f83390dd778686
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 15:37:54 -0600

    Project setup.
    Import into Eclipse.
    Convert to Maven project.
    Add JUnit 4.10 dependency.
    Move sources to proper packages.
    Add NotImplementedException.
    Stub out Atbash class.

---
+++ src/main/java/atbashcipher/Atbash.java
@@ -0,0 +1,14 @@
+package atbashcipher;
+
+import exceptions.NotImplementedException;
+
+public class Atbash
+{
+	public static String decode(String cipherText) {
+		throw new NotImplementedException();
+	}
+	
+	public static String encode(String plainText) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/atbashcipher/AtbashTest.java
+++ src/test/java/atbashcipher/AtbashTest.java
@@ -1,3 +1,4 @@
+package atbashcipher;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
