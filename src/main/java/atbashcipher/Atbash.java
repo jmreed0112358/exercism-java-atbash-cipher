960f9469e7f6de124b221bf3a997a887d6906e77
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 16:05:00 -0600

    Stub out Atbash.

--- src/main/java/atbashcipher/Atbash.java
+++ src/main/java/atbashcipher/Atbash.java
@@ -4,11 +4,46 @@ import exceptions.NotImplementedException;
 
 public class Atbash
 {
+	private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+	private static final String reverseAlphabet = "zyxwvutsrqponmlkjihgfedcba";
+
+	/**
+	 * Decodes a string encoded by an atbash cippher.
+	 * Remove all spaces from the input.
+	 * There should be no spaces in the output.
+	 * @param cipherText
+	 * @return
+	 */
 	public static String decode(String cipherText) {
 		throw new NotImplementedException();
 	}
 	
+	/**
+	 * Encodes a string by atbash cipher.
+	 * Spaces should be removed from the input.
+	 * When building the output, the output should be groups of five letters, separated by a spaces.
+	 * @param plainText
+	 * @return
+	 */
 	public static String encode(String plainText) {
+		throw new NotImplementedException();
+	}
+	
+	/**
+	 * Given a encoded letter, get the correct decoded letter.
+	 * @param letter
+	 * @return
+	 */
+	private static Character getDecodedLetter(Character letter ) {
+		throw new NotImplementedException();
+	}
+	
+	/**
+	 * Given a letter, give the correct decoded letter;
+	 * @param letter
+	 * @return
+	 */
+	private static Character getEncodedLetter(Character letter) {
 		throw new NotImplementedException();
 	}
 }
